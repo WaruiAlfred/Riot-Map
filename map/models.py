4947986987040454f72addb013e76b353a1fdf90
Alfred Warui <alfredwarui17@gmail.com>
2021-10-26 10:28:37 +0300

    create riot and profile models and register them

--- map/models.py
+++ map/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+#Profile model
+class Profile(models.Model): 
+  user = models.OneToOneField(User,on_delete=models.CASCADE)
+  contact = models.CharField(max_length=10)
+  country = models.CharField(max_length=50)
+  location = models.CharField(max_length=30)
+  important_areas = models.TextField()
+  profile_pic = CloudinaryField('profile')
+  
+  def __str__(self):
+    return self.user.username
+
+#Riot model
+class Riot(models.Model): 
+  user = models.ForeignKey(User,on_delete=models.CASCADE)
+  location = models.CharField(max_length=30)
+  date_posted = models.DateField(auto_now_add=True)
+  time_riot_started = models.TimeField()
+  riot_status = models.CharField(max_length=40) #ended or happening
+  nature_of_riot = models.CharField(max_length=40) #peaceful or violent
+  image = CloudinaryField('image')
+  
+  def __str__(self):
+    return self.location
