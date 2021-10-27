9486565582aad9d23368290469b0b3c183636226
Alfred Warui <alfredwarui17@gmail.com>
2021-10-27 23:29:44 +0300

    modified Profiles model and its registration in the admin file

--- map/admin.py
+++ map/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
 from .models import Profile,Riot
+from django.conf import settings
 
 # Register your models here.
-admin.site.register(Profile)
+# admin.site.register(Profile)
 admin.site.register(Riot)
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin): 
+  list_display = ('user','contact','country','location','latitude','longitude','important_areas','profile_pic')
+  search_fields = ('location',)
+  
+  fieldsets = (
+    (None,{
+      'fields':('user','contact','country','location','latitude','longitude','important_areas','profile_pic',)
+    }),
+  )
+  class Media: 
+    if hasattr(settings,'GOOGLE_MAPS_API_KEY') and settings.GOOGLE_MAPS_API_KEY: 
+      css = {
+        'all':('css/styles.css',),
+      }
+      js = (
+        'https://maps.googleapis.com/maps/api/js?key={}'.format(settings.GOOGLE_MAPS_API_KEY),
+        'js/main.js',
+      )
